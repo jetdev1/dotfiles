16bfc874595ed20ce902cc50008d44244b21b47f
Lim Jit <limjit30@gmail.com>
2022-10-12 17:51:25 +0800

    Added config for Typescript

---
+++ nvim/after/ftplugin/Typescript.lua
@@ -0,0 +1,3 @@
+vim.opt.shiftwidth = 2               -- Size of indent
+vim.opt.softtabstop = 2              -- Number of spaces used in place of <tab>
+vim.opt.tabstop = 2                  -- Length of \t character
